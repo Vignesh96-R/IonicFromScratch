54790673369362392fdb00351da2043dab5eef9f
vignesh <vignesh.rr@mavens-i.com>
2024-08-03 13:13:22 +0530

    creating ionic project without angular, vue, react

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('my-button');
+
+  button.addEventListener('click', () => {
+    alert('Button clicked!');
+  });
+});
